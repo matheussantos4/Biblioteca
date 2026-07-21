72aa952a15e5bf51531e02e853bd8443a6d4fd25
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-21 19:30:02 -0300

    feat: Desenvolvimento da classe Aluno.

--- Sistema/Aluno.java
+++ Sistema/Aluno.java
@@ -1,4 +1,31 @@
 package Sistema;
 
 public class Aluno {
+    private String nome;
+    private int numPessoa;
+
+    public int getNumPessoa() {
+        return numPessoa;
+    }
+
+    public void setNumPessoa(int numPessoa) {
+        if (numPessoa > 0) {
+            this.numPessoa = numPessoa;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isBlank()) {
+            this.nome = nome;
+        }
+    }
+
+    public Aluno(String nome, int numPessoa) {
+        setNome(nome);
+        setNumPessoa(numPessoa);
+    }
 }
