49f2ea5f26527b2c041bf0b4869ca3097a4a908c
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-21 19:03:58 -0300

    feat:desenvolvimento da classe Livro.

---
+++ Sistema/Livro.java
@@ -0,0 +1,31 @@
+package Sistema;
+
+public class Livro {
+    private String nome;
+    private int numCadastro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isBlank()) {
+        this.nome = nome;
+        }
+    }
+
+    public int getNumCadastro() {
+        return numCadastro;
+    }
+
+    public void setNumCadastro(int numCadastro) {
+        numCadastro = numCadastro;
+    }
+
+    public Livro (String nome, int numCadastro) {
+
+        setNome(nome);
+        setNumCadastro(numCadastro);
+
+    }
+}
