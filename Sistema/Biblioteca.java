3865b73f5c67ed28d9a89e58cda0e6565883ff2d
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-25 20:44:52 -0300

    feat: Desenvolvimento classe biblioteca.

--- Sistema/Biblioteca.java
+++ Sistema/Biblioteca.java
@@ -1,4 +1,35 @@
 package Sistema;
 
 public class Biblioteca {
+
+    private int quantidadeAlunos, quantidadeLivros;
+
+    private Livro[] acervo = new Livro[20];
+    private Aluno[] alunos = new Aluno[10];
+
+    public int getQuantidadeAlunos() {
+        return quantidadeAlunos;
+    }
+
+    public void setQuantidadeAlunos(int quantidadeAlunos) {
+        this.quantidadeAlunos = quantidadeAlunos;
+    }
+
+    public int getQuantidadeLivros() {
+        return quantidadeLivros;
+    }
+
+    public void setQuantidadeLivros(int quantidadeLivros) {
+        this.quantidadeLivros = quantidadeLivros;
+    }
+
+    public void adicionarlivro(Livro livro) {
+        if (quantidadeLivros < 20) {
+            acervo[quantidadeLivros] = livro;
+            quantidadeLivros++;
+        }
+
+
+    }
+
 }
