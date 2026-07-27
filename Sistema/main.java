4be8ff49c117083a09396d38c1cb4f8c4fb3ae86
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-27 18:42:49 -0300

    Feat: Alteração funcionamento ID para string.

--- Sistema/main.java
+++ Sistema/main.java
@@ -7,9 +7,12 @@ public class main {
 
         Scanner sc = new Scanner(System.in);
 
+
         Biblioteca b1 = new Biblioteca();
-        Livro liv1 = new Livro("Crime e castigo", 01);
+        Livro liv1 = new Livro("Crime e castigo.", "01");
         Aluno a1 = new Aluno("Matheus", 01);
+
+        b1.adicionarLivro(liv1);
 
         int opcao = 0;
 
@@ -26,20 +29,65 @@ public class main {
                 System.out.println("Opção inválida.");
 
             } else if (opcao == 1) {
-                System.out.println("Digite o nome do livro.");
-                String nome = sc.nextLine();
-                sc.nextLine();
+                System.out.print("Digite o nome do livro: ");
 
-                System.out.println("Digite o ID do livro.");
-                int ID = sc.nextInt();
+                sc.nextLine();
+                String nome = sc.nextLine();
+
+                if (nome == null || nome.isBlank()) {
+                    do {
+                        System.out.println("Nome inválido.");
+                        nome = sc.nextLine();
+
+                    } while (nome == null || nome.isBlank());
+                }
+
+                System.out.print("Digite o ID do livro: ");
+                
+                String ID = sc.nextLine();
+
+                if (ID == null || ID.isBlank()) {
+                    do {
+                        System.out.println("ID inválido.");
+                        ID = sc.nextLine();
+
+                    } while (ID == null || ID.isBlank());
+                }
 
                 Livro livronovo = new Livro(nome, ID);
                 b1.adicionarLivro(livronovo);
+
                 System.out.println("Livro adicionado!");
+                System.out.println();
 
             } else if (opcao == 2) {
+                b1.listarLivros();
+                System.out.println();
+                System.out.println();
+
+            } else if (opcao == 3) {
+                sc.nextLine();
+                System.out.print("Digite o nome do aluno: ");
+                String nomealuno = sc.nextLine();
+
+                System.out.print("ID aluno: ");
+                int IDaluno = sc.nextInt();
+
+                Aluno alunonovo = new Aluno(nomealuno, IDaluno);
+                b1.adicionarAluno(alunonovo);
+
+                System.out.println("Aluno adicionado!");
+
+            } else if (opcao == 4) {
+                b1.listarAlunos();
+                System.out.println();
+                System.out.println();
 
             }
+
         } while (opcao != 5);
+        System.out.println("Saindo...");
+
+        sc.close();
     }
 }
